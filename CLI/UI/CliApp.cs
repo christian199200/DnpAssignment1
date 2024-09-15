4c3895831414397c4a6332a17c77d9a14da1f99f
CHRISTIAN <tchoumkeunya@gmail.com>
2024-09-15 20:36:57 +0200

    init

--- CLI/UI/CliApp.cs
+++ CLI/UI/CliApp.cs
@@ -1,6 +1,16 @@
+using RepositoryContracts;
+
 namespace CLI.UI;
 
 public class CliApp
 {
-    
+    public CliApp(IUserRepository userRepository, ICommentRepository commentRepository, IPostRepository postRepository)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task StartAsync()
+    {
+        throw new NotImplementedException();
+    }
 }
