d4e88bc25a325f272d3f3b94c0c666e93b96f82c
CHRISTIAN <tchoumkeunya@gmail.com>
2024-09-23 13:59:38 +0200

    init

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -4,9 +4,9 @@ using InMomoryRepository;
 using RepositoryContracts;
 
 Console.WriteLine("Starting CLI app...");
-IUserRepository userRepository = new UserFileRepository();
-ICommentRepository commentRepository = new CommentFileRepository();
-IPostRepository postRepository = new PostFileRepository();
+IUserRepository userRepository = new UserFileRepository();  // old: UserInMemoryRepository
+ICommentRepository commentRepository = new CommentFileRepository();  // old: CommentInMemoryRepository
+IPostRepository postRepository = new PostFileRepository();  // old: PostInMemoryRepository
 
 CliApp cliApp = new CliApp(userRepository, commentRepository, postRepository);
 await cliApp.StartAsync();
